83a881ae3e25d628cca7ec5c165191f5e721207f
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-26 14:54:32 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int var1 = 5;
+        int var2 = 7;
+        int var3 = var1 + var2;
+        System.out.println("the result of adding the two variables is: " +var3);
+
+        double var4 = 5.005;
+        double var5 = 7.007;
+        double var6 = var4 + var5;
+        System.out.println("the result of adding the two doubles is: " +var6);
+
+        int var7 = 8;
+        double var8 = 9.009;
+        float var9 = (float) (var7 + var8);
+        System.out.println("the result of adding the integer and double together is a floating number: " +var9);
+
+
+    }
+}
